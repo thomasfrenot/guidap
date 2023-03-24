5b6ccf65bd0b28e6a9658cef7ee2f80efcdf3fbc
Thomas Frenot <tfrenot@gmail.com>
2023-03-24 23:49:17 +0100

    Ajout authentification JWT

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -4,11 +4,20 @@ namespace App\DataFixtures;
 
 use App\Entity\Activity;
 use App\Entity\RecreationPark;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHacher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHacher)
+    {
+        $this->passwordHacher = $passwordHacher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         /* Activities */
@@ -41,6 +50,43 @@ class AppFixtures extends Fixture
         $recreationPark->setWebsite('https://www.wampark.fr/toulouse-sesquieres/toulouse-sesquieres-tarifs/');
         $recreationPark->addActivity($this->getReference('Wakeboard'));
         $manager->persist($recreationPark);
+
+        $recreationPark = new RecreationPark();
+        $recreationPark->setName('Wam Park 2');
+        $recreationPark->setAddress('Ici on s\éclate avec des bolides à pagaie');
+        $recreationPark->setCity('Toulouse');
+        $recreationPark->setZipcode(31200);
+        $recreationPark->setDescription('Allez viens !');
+        $recreationPark->setWebsite('https://www.wampark.fr/toulouse-sesquieres/toulouse-sesquieres-tarifs/');
+        $recreationPark->addActivity($this->getReference('Canoë'));
+        $manager->persist($recreationPark);
+
+        $recreationPark = new RecreationPark();
+        $recreationPark->setName('Wam Park 3');
+        $recreationPark->setAddress('Base de Loisirs Sesquières, All. des Foulques');
+        $recreationPark->setCity('Toulouse');
+        $recreationPark->setZipcode(31200);
+        $recreationPark->setDescription('Viens faire du wakeboard à Toulouse');
+        $recreationPark->setWebsite('https://www.wampark.fr/toulouse-sesquieres/toulouse-sesquieres-tarifs/');
+        $recreationPark->addActivity($this->getReference('Wakeboard'));
+        $manager->persist($recreationPark);
+
+        $recreationPark = new RecreationPark();
+        $recreationPark->setName('Wam Park 4');
+        $recreationPark->setAddress('Base de Loisirs Sesquières, All. des Foulques');
+        $recreationPark->setCity('Toulouse');
+        $recreationPark->setZipcode(31200);
+        $recreationPark->setDescription('Viens faire du wakeboard à Toulouse');
+        $recreationPark->setWebsite('https://www.wampark.fr/toulouse-sesquieres/toulouse-sesquieres-tarifs/');
+        $recreationPark->addActivity($this->getReference('Wakeboard'));
+        $manager->persist($recreationPark);
+
+        /* Users */
+        $user = new User();
+        $user->setUsername('guidap');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordHacher->hashPassword($user, 'welcome-2023'));
+        $manager->persist($user);
 
         $manager->flush();
     }
